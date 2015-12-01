0f1227debb9b3bbf9ecd2dee4ae2d03e1befbe47
zerotypos-found <zero.typos.found@gmail.com>
2015-12-01 11:57:26 +0900

    Add 2015/C

---
+++ 2015/C/c1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+
+printf("こんにちは世界");
+
+return 0;
+}
+
---
+++ 2015/C/c3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+	int x=2,i,sum=1;
+	for(i=0;i<5;i++){
+		sum*=x;
+	}
+	printf("%d¥n",sum);
+	return 0;
+}
