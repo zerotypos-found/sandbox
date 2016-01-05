17b9285805441da5c2ea5aa656bfa895fd4fc20b
zerotypos-found <zero.typos.found@gmail.com>
2016-01-05 11:54:50 +0900

    Add 2015/E

---
+++ 2015/E/e1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void){
+	int n=0,i;
+	for(i=1;i<11;i++){
+		n+=i;
+	}
+	printf("%d\n",n);
+	return 0;
+}
---
+++ 2015/E/e2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+	puts("A happy new year")!
+	return 0;
+}
